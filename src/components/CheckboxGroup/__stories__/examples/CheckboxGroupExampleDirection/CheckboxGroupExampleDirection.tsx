f95012b1e5bee63fdc4c3d63051401faf50ca492
Anya Ovsyannikova <arhayka@gmail.com>
2021-07-12 11:24:13 +0300

    docs(Checkbox): edit docs
    issue -  816

---
+++ src/components/CheckboxGroup/__stories__/examples/CheckboxGroupExampleDirection/CheckboxGroupExampleDirection.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+// import { items } from '../../../__mocks__/data.mock';
+// import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+// import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+// import { CheckboxGroup } from '../../../CheckboxGroup';
+//
+// type Item = {
+//   name: string;
+//   disabled?: boolean;
+// };
+
+export function CheckboxGroupExampleRow() {
+  // const [value, setValue] = React.useState<Item[] | null>(null);
+
+  return (
+    <div />
+    // <StoryBookExample className={cnDocsDecorator('Section')}>
+    //   <CheckboxGroup
+    //     value={value}
+    //     items={items}
+    //     getLabel={(item) => item.name}
+    //     getDisabled={(item) => item.disabled}
+    //     onChange={({ value }) => setValue(value)}
+    //     name="CheckboxGroup2"
+    //     direction="row"
+    //   />
+    // </StoryBookExample>
+  );
+}
+export function CheckboxGroupExampleColumn() {
+  // const [value, setValue] = React.useState<Item[] | null>(null);
+
+  return (
+    <div />
+    // <StoryBookExample className={cnDocsDecorator('Section')}>
+    //   <CheckboxGroup
+    //     value={value}
+    //     items={items}
+    //     getLabel={(item) => item.name}
+    //     getDisabled={(item) => item.disabled}
+    //     onChange={({ value }) => setValue(value)}
+    //     name="CheckboxGroup1"
+    //     direction="column"
+    //   />
+    // </StoryBookExample>
+  );
+}
